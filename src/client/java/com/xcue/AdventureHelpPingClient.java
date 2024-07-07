a2af4ce20b2c8b9ec211a3dd87de01ad3d21cb91
xCue <daniel@ohrin.com>
2024-07-07 14:32:13 -0500

    Initial Commit

---
+++ src/client/java/com/xcue/AdventureHelpPingClient.java
@@ -0,0 +1,10 @@
+package com.xcue;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdventureHelpPingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
